ebfcd218cd0964ba9d786898b9c3b13ec7612abc
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-04-01 11:21:11 +0200

    bleh

--- Week 4 Assignments/W04.2.C01 (Method overloading)/PersonalInformation.cs
+++ Week 4 Assignments/W04.2.C01 (Method overloading)/PersonalInformation.cs
@@ -1,27 +1,27 @@
-static class PersonalInformation
+public static class PersonalInformation
 {
-    static void PrintName(string input)
+    public static void PrintName(string input)
     {
         Console.WriteLine(input);
     }
 
-    static void PrintName(string input1, string input2)
+    public static void PrintName(string input1, string input2)
     {
         Console.WriteLine($"{input1} {input2}");
     }
 
-    static void PrintName(char input1, string input2)
+    public static void PrintName(char input1, string input2)
     {
         Console.WriteLine($"{input1}. {input2}");
     }
 
-    static int IncreaseSalary(int int1)
+    public static int IncreaseSalary(int int1)
     {
         int1 += 100;
         return int1;
     }
 
-    static double IncreaseSalary(int int1, double double1)
+    public static double IncreaseSalary(int int1, double double1)
     {
         double intReturn = int1 * (1 + double1);
         return intReturn;
