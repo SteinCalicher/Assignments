2f0be3037aa58ed4e3747135cc1802f32c942d19
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-04-01 09:17:38 +0200

    Added the classes

---
+++ Week 4 Assignments/W04.1.C07 (Files and specific exception handling)/Car.cs
@@ -0,0 +1,10 @@
+
+public class Car
+{
+    public string Brand;
+    public int Mileage;
+
+    public Car(string brand) => Brand = brand;
+
+    public void Drive() => Mileage++;
+}
--- Week 4 Assignments/W04.1.C07 (Files and specific exception handling)/Program.cs
+++ Week 4 Assignments/W04.1.C07 (Files and specific exception handling)/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+class Program
+{
+    public static void Main()
+    {
+        List<Person> ListOfPeople = FileReader();
+        foreach(Person person in ListOfPeople)
+        {
+            foreach(Car car in person.OwnedCars)
+            {
+                for(int i = 0; i < 5;i++)
+                {
+                    car.Drive();
+                }
+            }
+        }
+        FileWriter(ListOfPeople);
+    }
+
+    public static List<Person> FileReader()
+    {
+        var FileName = @"People.json";
+        try
+        {    
+            using (StreamReader sr = new(FileName))
+            {
+                string JsonString = sr.ReadToEnd();
+                List<Person> ListOfPersons = JsonConvert.DeserializeObject<List<Person>>(JsonString)!;
+                return ListOfPersons;
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return null;
+        }
+
+    }
+
+    public static void FileWriter(List<Person> Input)
+    {
+        var FileName = @"People.json";
+        try
+        {
+            using (StreamWriter sw = new(FileName))
+            {
+                var List2Json = JsonConvert.SerializeObject(Input);
+                sw.Write(List2Json);
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
