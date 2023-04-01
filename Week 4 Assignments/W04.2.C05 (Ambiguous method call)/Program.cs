60fc3b28a12071aa174e6bc5d4e6938919ad0116
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-04-01 18:23:55 +0200

    blweh

--- Week 4 Assignments/W04.2.C05 (Ambiguous method call)/Program.cs
+++ Week 4 Assignments/W04.2.C05 (Ambiguous method call)/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        var products = Factory.Produce("Tooth Paste", 5, 2.0);
+        double revenue = SellProducts(products);
+        Console.WriteLine($"Revenue from selling: {revenue}");
+    }
+
+    public static double SellProducts(List<Product> products)
+    {
+        double total = 0;
+        foreach (Product product in products)
+            total += product.Price;
+        return total;
+    }
+}
