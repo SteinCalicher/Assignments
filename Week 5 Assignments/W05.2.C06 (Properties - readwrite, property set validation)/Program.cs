b6e4db583e73e4d77a83b6213785f2bc1f48b83a
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-04-09 18:57:42 +0200

    Made W05.2.C06 (Doesnt work yet)

--- Week 5 Assignments/W05.2.C06 (Properties - readwrite, property set validation)/Program.cs
+++ Week 5 Assignments/W05.2.C06 (Properties - readwrite, property set validation)/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        Temperature temperature = new()
+        {
+            Celsius = 100
+        };
+        PrintTemperature(temperature);
+
+        temperature.Kelvin = 100;
+        PrintTemperature(temperature);
+
+        temperature.Celsius = -274;
+        PrintTemperature(temperature);
+
+        temperature.Kelvin = -1;
+        PrintTemperature(temperature);
+
+        temperature.Celsius = -273.15;
+        PrintTemperature(temperature);
+    }
+
+    private static void PrintTemperature(Temperature temperature)
+    {
+        Console.WriteLine("Temperature in Celsius: " + (int)temperature.Celsius);
+        Console.WriteLine("Temperature in Kelvin: " + (int)temperature.Kelvin);
+        Console.WriteLine();
+    }
+}
