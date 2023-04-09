34aabe906e1e9cf9c1e2d6915dbbce93599db0d1
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-04-09 12:10:03 +0200

    Made W01.1.C06

---
+++ Week 5 Assignments/W05.1.C06 (Derived and derived further)/HarleyDavidson.cs
@@ -0,0 +1,7 @@
+public class HarleyDavidson : Cruiser
+{
+    public HarleyDavidson(string model, int year): base("Harley Davidson", model,year, 68)
+    {}
+
+    public override string Ride() => base.Ride() + $" and with pride.";
+}
