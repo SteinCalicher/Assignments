5247e2ece7df0d13ed96e007e24b0860a4b1bc96
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-04-11 10:00:55 +0200

    Made W06.2.C05

---
+++ Week 6 Assignments/W06.2.C05 (using new methods)/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Employee e = new("John", "Doe", "john.doe@example.com");
+        Manager m = new("Jane", "Doe", "jane.doe@example.com", "Sales");
+        SalesPerson s = new("Bob", "Smith", "bob.smith@example.com", 100000);
+        List<Employee> workers = new() {e, m, s};
+
+        e.PrintEmployeeInfo();
+        
+        Employee em = m as Employee;
+        em.PrintEmployeeInfo();
+
+        Employee es = s as Employee;
+        es.PrintEmployeeInfo();
+
+        e.PrintEmployeeInfo();
+        m.PrintEmployeeInfo();
+        s.PrintEmployeeInfo();
+
+
+
+
+    }
+}
